67fcc8dce949ffaea8cb9bd37d843fb9ab3ea26e
kaushik-ladumor <kaushikladumor80@gmail.com>
2025-06-10 15:54:17 +0530

    Describe your change

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,19 @@
 const express = require('express');
-const router = express.Router();
+const route = express.Router();
 const User = require('../models/user');
 const { isAdmin } = require('../middleware/Admin');
 const { deleteUserAndRelatedData } = require('../controllers/auth');
 
 // Show all users
-router.get('/admin', isAdmin, async (req, res) => {
+route.get('/admin', isAdmin, async (req, res) => {
     const users = await User.find({});
     res.render('admin/HandleUser', { users });
 });
 
 // POST route (optional)
-router.post('/users/:id/delete', isAdmin, deleteUserAndRelatedData);
+route.post('/users/:id/delete', isAdmin, deleteUserAndRelatedData);
 
 // DELETE route (used by modal form)
-router.delete('/admin/users/:id', isAdmin, deleteUserAndRelatedData);
+route.delete('/admin/users/:id', isAdmin, deleteUserAndRelatedData);
 
-module.exports = router;
+module.exports = route;
